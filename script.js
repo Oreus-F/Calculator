44a10b2bd4c23b0846d6c8910f69f115a33e9780
Oreus-F <fourniol.lucas@gmail.com>
2024-11-23 16:35:13 +0100

    add basic calculation functions

---
+++ script.js
@@ -0,0 +1,17 @@
+
+
+const add = function(a, b){
+    return a + b;
+};
+
+const sub = function(a, b){
+    return a - b;
+};
+
+const multiply = function(a, b){
+    return a * b;
+};
+
+const divide = function(a, b){
+    return a / b;
+};
