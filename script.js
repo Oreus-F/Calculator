b3741c26a2ffd0706ceb0aea419c6e5e87fe313b
Oreus-F <fourniol.lucas@gmail.com>
2024-11-28 20:00:00 +0100

    Setup the number displayed on the calculator

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
 let number1;
 let number2;
 let operator;
-let displayedNumber = [2, 1];
-let str = displayedNumber.join("")
+let displayedNumber = "";
+
 
 const digitBox = document.querySelector("#digitBox");
 const resultDisplay = document.querySelector("#resultDisplay");
@@ -28,7 +28,10 @@ const operate = function(action, firstNumber, secondNumber){
     return action(firstNumber, secondNumber);
 };
 
-resultDisplay.textContent = str;
+const changeNumbersDisplayed = function(){
+    let str = displayedNumber.join("");
+    resultDisplay.textContent = str;
+};
 
 digitBox.addEventListener("click", (e) => {
     let target = e.target;
@@ -38,52 +41,85 @@ digitBox.addEventListener("click", (e) => {
         
 
         case "clear":
-            
+            displayedNumber = "";
+            resultDisplay.textContent = displayedNumber;
             break;
 
 
         case "del":
+
             break;
                     
 
         case "1":
-
+            displayedNumber += target.id;
+            resultDisplay.textContent = displayedNumber;
             break;
 
     
         case "2":
+            displayedNumber += target.id;
+            resultDisplay.textContent = displayedNumber;
             break;
 
     
         case "3":
+            displayedNumber += target.id;
+            resultDisplay.textContent = displayedNumber;
             break;
         
     
         case "4":
+            displayedNumber += target.id;
+            resultDisplay.textContent = displayedNumber;
             break;
 
         
         case "5":
+            displayedNumber += target.id;
+            resultDisplay.textContent = displayedNumber;
             break;
         
     
         case "6":
+            displayedNumber += target.id;
+            resultDisplay.textContent = displayedNumber;
             break;
             
         
         case "7":
+            displayedNumber += target.id;
+            resultDisplay.textContent = displayedNumber;
             break;
          
             
         case "8":
-          break;
+            displayedNumber += target.id;
+            resultDisplay.textContent = displayedNumber;
+            break;
             
         
         case "9":
+            displayedNumber += target.id;
+            resultDisplay.textContent = displayedNumber;
             break;
 
         
         case "0":
+            displayedNumber += target.id;
+            resultDisplay.textContent = displayedNumber;
+            break;
+
+        case "comma":
+            displayedNumber = displayedNumber.split('');
+            if (displayedNumber.includes(".")) {
+                displayedNumber = displayedNumber.join('');
+                break};
+            displayedNumber = displayedNumber.join('');
+            displayedNumber += ".";
+            resultDisplay.textContent = displayedNumber;
             break;
     }
-})
+});
+
+resultDisplay.textContent = displayedNumber;
